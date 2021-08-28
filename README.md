0f46cfc2a9ce0e7f7af3b744e23a80efd2f84b14
Eric Lemieux <eric@lemieuxdev.com>
2021-08-28 10:39:58 -0400

    Server returns hello world

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Rust Web Server
 
-Experimental web server based on the final chapter of 'The Rust Programming Language' book.
+Experimental web server based on the [final chapter of 'The Rust Programming Language' book](https://doc.rust-lang.org/stable/book/ch20-00-final-project-a-web-server.html).
 
 This project serves as a playground of sorts for myself, don't expect this to work beyond the most basic of use cases.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use std::io::prelude::*;
+use std::net::{TcpListener, SocketAddr, TcpStream};
+
 fn main() {
-    println!("Hello, world!");
+    let port = 8080;
+
+    let listener = TcpListener::bind(SocketAddr::from(([127, 0, 0, 1], port))).unwrap();
+
+    eprintln!("Starting application on port {}", port);
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        handler(stream)
+    }
+}
+
+fn handler(mut stream: TcpStream) {
+    let mut buffer = [0; 512];
+
+    stream.read(&mut buffer).unwrap();
+
+    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+
+    let body = String::from("Hello world");
+    let response = format!("HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}", body.len(), body);
+
+    eprintln!("Response: {}", response);
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
